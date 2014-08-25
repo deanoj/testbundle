bf941588262d8c47965b7374979b6e2af6557e79
deanoj <deanoj@deanoj.co.uk>
2014-08-25 13:06:02 +0100

    FakeTest added

---
+++ Tests/FakeTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: deanoj
+ * Date: 25/08/2014
+ * Time: 13:05
+ */
+
+class FakeTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAdd()
+    {
+
+        $this->assertTrue(true);
+    }
+}
